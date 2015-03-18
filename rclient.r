92495a740ae18d57330cd1207c5932ed1b232776
longyanyue <451028552@qq.com>
2015-03-18 16:55:59 +1000

    .

--- rclient.r
+++ rclient.r
@@ -1,17 +1,7 @@
 client <- function(){
   con <- socketConnection(host="localhost", port = 6012, blocking=TRUE,server=FALSE, open="r+")
   while(TRUE){
-    
-                            
-   # f <- file("stdin")
-  #  open(f)
-  #  print("Enter text to be upper-cased, q to quit")
-  #  sendme <- readLines(f, n=1)
-  #  if(tolower(sendme)=="q"){
-  #    break
-  #  }
   sendme <- readline(prompt="Enter an integer: ")
-  sendme <- 
   if(sendme == "quit"){
     close(con)
     break
--- rserver.r
+++ rserver.r
@@ -6,7 +6,9 @@ server <- function(){
     data <- readLines(con, 1)
     if(length(data)==0){
       close(con)
-      break
+      con <- socketConnection(host="localhost", port = 6012, blocking=TRUE, server=TRUE, open="r+")
+      #continue
+     # break
     }
     print(data)
     response <- toupper(data) 
